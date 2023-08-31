683851d9bcff3b90a44aa5bf1caebc23e2da622c
jadeswanepoel <125471787+jadeswanepoel@users.noreply.github.com>
2023-09-01 00:12:34 +0200

    rewatching classes

--- scripts/main.js
+++ scripts/main.js
@@ -2,46 +2,65 @@
 // -------------------------------------------------------------------
 // trips array
 // ------------------------------------------------------------------
+
+
 const arrTrips = [
     {
         name: "Venice",
         price: "R27000",
         description: "very fun",
-        image: "card 1.png"
+        image: "card 1.png",
+        destinationDuration: "all",
+        origin: "Venice"
     },
     {
         name: "Croatia",
         price: "R31000",
         description: "very fun",
-        image: "card 2.png"
+        image: "card 2.png",
+        destinationDuration: "long",
+        origin: "Croatia"
     },
     {
-        name: "Bulagaria",
+        name: "Bulgaria",
         price: "R37000",
         description: "very fun",
-        image: "card 3.png"
+        image: "card 3.png",
+        destinationDuration: "short",
+        origin: "Bulgaria"
     },
     {
         name: "Venice",
         price: "R25000",
         description: "very fun",
-        image: "card 4.png"
+        image: "card 4.png",
+        destinationDuration: "single",
+        origin: "Venice"
+
     },
     {
         name: "Russia",
         price: "R33000",
         description: "very fun",
-        image: "card 5.png"
+        image: "card 5.png",
+        destinationDuration: "multi",
+        origin: "Russia"
     },
     {
         name: "Turkye",
         price: "R38000",
         description: "very fun",
-        image: "card 6.png"
+        image: "card 6.png",
+        destinationDuration: "round",
+        origin: "Turkye"
     },
 
 
  ];
+
+
+ let appliedFilter = "";
+
 
 
 
@@ -57,23 +76,40 @@ $(document).ready(function(){
 // trips page
 // ------------------------------------------------------------------
 
-loadTrips();
+loadTrips(arrTrips);
 
 
 });
 
 
 // --------------------------------------------------------------------------
-// load all trips
+// load trips
 // -------------------------------------------------------------------------
-function loadTrips() {
+function loadTrips(tripsToShow) {
 
-    console.log(arrTrips);
+    $("#tripsContainer").empty();
 
-    for (let i = 0; i < arrTrips.length; i++) {
-        const trips = arrTrips [i];
+    
 
-        console.log(trips.name);
+    for (let i = 0; i < tripsToShow.length; i++) {
+        const trips = tripsToShow [i];
+
+
+        $.ajax({
+            type:"GET",
+            url:"https://api.openweathermap.org/data/2.5/weather?q=" + trips.origin + "&appid=74d12cf5d5014d359a25eaa1e534a442",
+            success: function(data){
+    
+                data = data
+    
+                console.log(data);
+            }
+        }).done(function(){
+            
+
+            $(currentChild).find("#newTemp").text("OriginTemp:" + Math.round(tempData.main.temp -273) + "C");
+
+        })
 
 
         // 1 select trips container and add trips card
@@ -90,6 +126,7 @@ function loadTrips() {
 
         // hide description text from trips card
         $(currentChild).find("#descriptionText").hide();
+        $(currentChild).find("#newTemp").hide();
 
      
     };
@@ -97,6 +134,35 @@ function loadTrips() {
 
 
 };
+
+// --------------------------------------------------------------------------
+// when filter is selected
+// -------------------------------------------------------------------------
+$("input[name='filterRadio']").click(function(){
+    appliedFilter = $(this).attr('value');
+
+    console.log(appliedFilter);
+    filterSortTrips();
+
+});
+
+function filterSortTrips(){
+  
+    let filteredSortedArrTrips = [];
+
+    // filtering trips
+    if (appliedFilter){
+        filteredSortedArrTrips = arrTrips.filter(trips => trips.longDuration == appliedFilter)
+    } else {
+        filteredSortedArrTrips = arrTrips;
+    }
+
+    console.log(filteredSortedArrTrips);
+
+    loadTrips(filteredSortedArrTrips);
+}
+
+
 
 
 
@@ -110,10 +176,29 @@ $("#tripsContainer").on('click', '.card', function() {
 $(this).find("#priceText").toggle();
 
 $(this).find("#descritionText").toggle();
+$(this).find("#newTemp").toggle();
 
 // resize image to fit original content
 $(this).find("card-img-top").toggleClass("small");
 
+
+});
+
+
+$(document).ready(function(){
+
+    $.ajax({
+        type:"GET",
+        url:"https://api.openweathermap.org/data/2.5/weather?q=Venice&appid=74d12cf5d5014d359a25eaa1e534a442",
+        success: function(data){
+
+            data = data
+
+            console.log(data);
+        }
+    }).done(function(){
+        $("#tempdata").html(tempData.main.temp)
+    })
 })
 
 
